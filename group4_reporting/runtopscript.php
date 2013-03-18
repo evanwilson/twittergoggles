f0554636f4e3cf2b4ec2128ad2d1ae4a6acfee02
Evan Wilson <ecw39@drexel.edu>
2013-03-17 23:32:02 -0400

    Final changes

--- group4_reporting/runtopscript.php
+++ group4_reporting/runtopscript.php
@@ -1,4 +1,57 @@
 <?php
+echo '<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Page</title>
+  <link href="bootstrap.css" rel="stylesheet">
+
+
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+</head>
+<body>    
+
+	<!-- Navbar
+    ================================================== -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="">
+                <a href="/twittergoggles/index.html">Home</a>
+              </li>
+			  <li class="active">
+                <a href="/twittergoggles/group4_reporting/reporting.html">Visualization/Reporting</a>
+              </li>
+              
+              <li class="">
+                <a href="/twittergogglesgroup3_administration/Sprint3/index.php">Administration</a>
+              </li>
+              <li class="">
+                <a href="/twittergogglesgroup5_researchercol/sprint3/researcher.php">Research</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+        <br><br>';
 
 //Total Number of Tweets for All Specific Jobs
 
@@ -30,6 +83,8 @@ $CSVarray = array();
 
 
 //Sets up table
+
+echo '<html><head></head><link href="/twittergoggles/bootstrap.css" rel="stylesheet">';
 echo "<table border='1'
 <tr>
 <th>Job ID</th>
@@ -117,7 +172,7 @@ function outputCSV($data) {
     fclose($file);
 }
 
-
+echo "</html>";
 
 mysql_close($link);
 
